247f687236fd5fb014ecce6bec0f2b083fe81d85
Lucas Martins <lucaslealm@gmail.com>
2024-03-24 13:55:18 -0500

    feat(fiap-hackathon): adds authentication to create order endpoint

--- tests/integration/presentation/test_record_router.py
+++ tests/integration/presentation/test_record_router.py
@@ -1,28 +1,26 @@
-from datetime import datetime, timezone
 from unittest import IsolatedAsyncioTestCase
 
-from freezegun import freeze_time
 from starlette import status
 
 from app.domain.record.record_type import RecordType
 from app.infrastructure.container import ApplicationContainer
 from tests.factories.domain_factories import UserFactory
+from tests.helpers.auth_token_helper import AuthTokenHelper
 
 
-@freeze_time("2024-03-23 10:00:00")
 class TestRecordRouter(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.base_route = "/records"
         self.user = UserFactory()
-        self.now_utc = datetime.now(timezone.utc)
 
         self.user_repository = ApplicationContainer.user_repository()
         self.record_repository = ApplicationContainer.record_repository()
 
     async def test_post_WHEN_called_RETURNS_201(self):
         await self.user_repository.save(self.user)
+        auth_header = AuthTokenHelper.get_valid_token_headers(self.user)
 
-        response = self.client.post(url=f"{self.base_route}/{self.user.id}")
+        response = self.client.post(url=f"{self.base_route}", headers=auth_header)
 
         persisted_record = await self.record_repository.get_user_todays_last_record(user_id=self.user.id)
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
@@ -30,5 +28,13 @@ class TestRecordRouter(IsolatedAsyncioTestCase):
         self.assertEqual(RecordType.IN, persisted_record.type)
 
     async def test_post_WHEN_user_not_exists_RETURNS_404(self):
-        response = self.client.post(url=f"{self.base_route}/{self.user.id}")
+        auth_header = AuthTokenHelper.get_valid_token_headers(self.user)
+
+        response = self.client.post(url=f"{self.base_route}", headers=auth_header)
+
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+    async def test_post_WHEN_user_not_authenticated_RETURNS_401(self):
+        response = self.client.post(url=f"{self.base_route}", headers=AuthTokenHelper.get_invalid_token_headers())
+
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
