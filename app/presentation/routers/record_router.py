d977f35d48c34a40afa77973884917b36662bb75
Lucas Martins <46767480+lucaslealm@users.noreply.github.com>
2024-03-24 17:02:02 -0500

    Feat/authenticate routers (#12)
    * feat(fiap-hackathon): adds auth_token_helper
    
    * feat(fiap-hackathon): adds authentication to create order endpoint
    
    * feat(fiap-hackathon): refactores record router

--- app/presentation/routers/record_router.py
+++ app/presentation/routers/record_router.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from uuid import UUID
 
 from dependency_injector.wiring import Provide, inject
@@ -6,14 +7,16 @@ from starlette import status
 
 from app.application.use_cases.record.create_record import CreateRecord
 from app.infrastructure.container import ApplicationContainer
+from app.presentation.helpers.authentication_helper import AuthenticationHelper
 from app.presentation.helpers.custom_router import APICustomRouter
 
 router = APICustomRouter(prefix="/records")
 
 
-@router.post("/{user_id}", status_code=status.HTTP_201_CREATED)
+@router.post("", status_code=status.HTTP_201_CREATED)
 @inject
 async def create_record(
-    user_id: UUID, use_case: CreateRecord = Depends(Provide[ApplicationContainer.create_record])
+    user_id: Annotated[UUID, Depends(AuthenticationHelper.get_current_user_id)],
+    use_case: CreateRecord = Depends(Provide[ApplicationContainer.create_record]),
 ) -> None:
     await use_case(user_id=user_id)
