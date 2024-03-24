457b2c35f19acfbe3dfe58648cf659cf810c40fb
Lucas Martins <lucaslealm@gmail.com>
2024-03-24 14:06:18 -0500

    feat(fiap-hackathon): refactores record router

--- app/presentation/routers/record_router.py
+++ app/presentation/routers/record_router.py
@@ -7,7 +7,7 @@ from starlette import status
 
 from app.application.use_cases.record.create_record import CreateRecord
 from app.infrastructure.container import ApplicationContainer
-from app.presentation.dependencies.authentication_dependencies import AuthenticationDependencies
+from app.presentation.helpers.authentication_helper import AuthenticationHelper
 from app.presentation.helpers.custom_router import APICustomRouter
 
 router = APICustomRouter(prefix="/records")
@@ -16,7 +16,7 @@ router = APICustomRouter(prefix="/records")
 @router.post("", status_code=status.HTTP_201_CREATED)
 @inject
 async def create_record(
-    user_id: Annotated[UUID, Depends(AuthenticationDependencies.get_current_user_id)],
+    user_id: Annotated[UUID, Depends(AuthenticationHelper.get_current_user_id)],
     use_case: CreateRecord = Depends(Provide[ApplicationContainer.create_record]),
 ) -> None:
     await use_case(user_id=user_id)
